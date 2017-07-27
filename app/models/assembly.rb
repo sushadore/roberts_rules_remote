d6ebe3ca64a7cafc2765bc9d2cd618d65aaf5406
Susha Dore <susha.dore@gmail.com>
2017-07-27 14:50:15 -0700

    add assembly class and pass testing

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -1,2 +1,7 @@
 class Assembly < ApplicationRecord
+  has_many :messages
+  belongs_to :user
+  validates :title, presence: true
+  validates :agenda, presence: true
+  validates :date, presence: true
 end
--- spec/models/assembly_spec.rb
+++ spec/models/assembly_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Assembly, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Assembly, type: :model do
+  it { should belong_to :user }
+  it { should have_many :messages}
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :agenda }
+  it { should validate_presence_of :date }
 end
