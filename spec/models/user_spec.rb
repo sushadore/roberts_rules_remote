88d7db32a58423015977bc839bf55f77dc373d87
Susha Dore <susha.dore@gmail.com>
2017-07-27 14:45:19 -0700

    add simplecov interface and add user class passing testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it { should have_many :assemblies }
+  it { should have_many :messages}
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :first_name }
 end
