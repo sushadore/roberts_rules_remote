402f5f1b940737b32db154421a779c5f23dc99eb
Susha Dore <susha.dore@gmail.com>
2017-07-27 14:48:15 -0700

    add user class and pass testing

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :assembly
+  validates :content, presence: true
+  after_create_commit { MessageBroadcastJob.perform_later self }
+
+# Potentially add timestamp to user messages
+#   def timestamp
+#     created_at.strftime('%H:%M:%S %d %B %Y')
+#   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Message, type: :model do
+  it { should belong_to :user}
+  it { should belong_to :assembly}
+  it { should validate_presence_of :content }
 end
